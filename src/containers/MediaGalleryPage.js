412f19fe1713892bd9eb51d6443397931f4d9ecb
Nikola Serdiuk <nikola.serdiuk@gmail.com>
2017-01-21 19:19:43 +0200

    Fixed MediaGalleryPage

--- src/containers/MediaGalleryPage.js
+++ src/containers/MediaGalleryPage.js
@@ -4,7 +4,7 @@ import { flickrImages, shutterStockVideos } from '../Api/api';
 class MediaGalleryPage extends Component {
   componentWillMount () {
     flickrImages('rain').then(images => console.log(images, 'Image'));
-    shutterStockVideos('rain').then(videos = console.log(videos, 'Video'));
+    shutterStockVideos('rain').then(videos => console.log(videos, 'Video'));
   }
 
   render() {
