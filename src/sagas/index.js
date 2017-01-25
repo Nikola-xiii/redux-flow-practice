f2b2c48b876de4c5cda92ded80e9ea27ca8f31b8
Nikola Serdiuk <nikola.serdiuk@gmail.com>
2017-01-25 16:29:56 +0200

    Fixed some problems

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -3,7 +3,6 @@ import watchSearchMedia from './watcher';
 
 // Here, we register our watcher saga(s) and export as a single generator
 // function (startForeman) as our root Saga.
-
 export default function* startForman() {
   yield fork(watchSearchMedia);
 }
--- src/sagas/mediaSaga.js
+++ src/sagas/mediaSaga.js
@@ -2,19 +2,20 @@ import { put, call } from 'redux-saga/effects';
 import { flickrImages, shutterStockVideos } from '../Api/api';
 import * as types from '../constants/actionTypes';
 
+// Responsible for searching media library, making calls to the API
+// and instructing the redux-saga middle ware on the next line of action,
+// for success or failure operation.
 export function* searchMediaSaga({ payload }) {
   try {
     const videos = yield call(shutterStockVideos, payload);
     const images = yield call(flickrImages, payload);
-
     yield [
-      put({types: types.SHUTTER_VIDEOS_SUCCESS, videos}),
-      put({types: types.SELECTED_VIDEO, video: videos[0] }),
-      put({types: types.FLICKR_IMAGES_SUCCESS, images}),
-      put({types: types.SELECTED_IMAGE, image: images[0] })
-    ]
-
+      put({ type: types.SHUTTER_VIDEOS_SUCCESS, videos }),
+      put({ type: types.SELECTED_VIDEO, video: videos[0] }),
+      put({ type: types.FLICKR_IMAGES_SUCCESS, images }),
+      put({ type: types.SELECTED_IMAGE, image: images[0] })
+    ];
   } catch (error) {
-    yield put({types: 'SEARCH_MEDIA_ERROR', error});
+    yield put({ type: 'SEARCH_MEDIA_ERROR', error });
   }
 }
