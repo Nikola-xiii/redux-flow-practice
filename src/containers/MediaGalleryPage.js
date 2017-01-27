43458fd7c81f523d7aea72d85956fc63f1001c7b
Nikola Serdiuk <nikola.serdiuk@gmail.com>
2017-01-27 15:43:26 +0200

    Create presentational components

--- src/containers/MediaGalleryPage.js
+++ src/containers/MediaGalleryPage.js
@@ -1,25 +1,84 @@
-import React, { Component } from 'react';
+import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
-import { searchMediaAction } from '../actions/mediaActions';
+import { searchMediaAction, selectImageAction, selectVideoAction } from '../actions/mediaActions';
+import PhotoPage from '../components/PhotoPage';
+import VideoPage from '../components/VideoPage';
+import '../styles/style.css';
 
 class MediaGalleryPage extends Component {
   // Dispatches *searchMediaAction*  immediately after initial rendering.
   // Note that we are using the dispatch method from the store to execute this task, courtesy of react-redux
+  constructor() {
+    super();
+
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleSelectImage = this.handleSelectImage.bind(this);
+    this.handleSelectVideo = this.handleSelectVideo.bind(this);
+  }
+
   componentDidMount() {
     this.props.dispatch(searchMediaAction('rain'));
   }
 
+  // Dispatches *selectImageAction* when any image is clicked
+  handleSelectImage(selectedImage) {
+    this.props.dispatch(selectImageAction(selectedImage));
+  }
+
+  // Dispatches *selectvideoAction* when any video is clicked
+  handleSelectVideo(selectedVideo) {
+    this.props.dispatch(selectVideoAction(selectedVideo));
+  }
+
+  handleSearch(event) {
+    event.preventDefault();
+
+    if (this.query !== null) {
+      this.props.dispatch(searchMediaAction(this.query.value));
+      this.query.value = '';
+    }
+  }
+
   render() {
-    console.log(this.props.images, 'Images');
-    console.log(this.props.videos, 'Videos');
-    console.log(this.props.selectedImage, 'SelectedImage');
-    console.log(this.props.selectedVideo, 'SelectedVideo');
-    return (<div> </div>)
+    const { images, selectedImage, videos, selectedVideo } = this.props;
+
+    return (
+      <div className="container-fluid">
+        {images ? <div>
+            <input
+              type="text"
+              ref={ref => (this.query = ref)}
+            />
+            <input
+              type="submit"
+              className="btn btn-primary"
+              value="Search Library"
+              onClick={this.handleSearch}
+            />
+            <div className="row">
+              <PhotoPage
+                images={images}
+                selectedImage={selectedImage}
+                onHandleSelectImage={this.handleSelectImage}
+              />
+              <VideoPage
+                videos={videos}
+                selectedVideo={selectedVideo}
+                onHandleSelectVideo={this.handleSelectVideo}
+              />
+            </div>
+          </div> : 'loading ....'}
+      </div>
+    );
   }
 }
 
 MediaGalleryPage.PropTypes = {
-  // Define your PropTypes here
+  images: PropTypes.array,
+  selectedImage: PropTypes.object,
+  videos: PropTypes.array,
+  selectedVideo: PropTypes.object,
+  dispatch: PropTypes.func.isRequired
 };
 
 // Subscribe component to redux store and merge the state into
